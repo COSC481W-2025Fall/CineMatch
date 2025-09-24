ffea2af1f378930a7ce1e0e7d318d281adebb0a7
Caleb Bujauskas <196842407+CalebBujauskas@users.noreply.github.com>
2025-09-23 21:49:41 -0400

    Changed: removed desc in search query in record.js
    issue #10

--- mern/server/routes/record.js
+++ mern/server/routes/record.js
@@ -12,13 +12,12 @@ router.get("/", async (req, res) => {
   try {
     const col = db.collection("movies");
 
-    const { title, name, year, rating, desc, genre } = req.query;
+    const { title, name, year, rating, genre } = req.query;
     const filter = {};
     const qName = name ?? title;                 
     if (qName) filter.name = { $regex: qName, $options: "i" };
     if (year)  filter.date = Number(year);
     if (rating) filter.rating = { $gte: Number(rating) };
-    if (desc)  filter.description = { $regex: desc, $options: "i" };
     if (genre)  filter.description = { $regex: genre, $options: "i" };
 
 
