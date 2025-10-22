757d7ec2e59b453a77f5c310ef06ba8054f49377
DuJuan <dj@DuJuans-Laptop.local>
2025-10-21 23:39:24 -0400

    Fixing the sticky and moving the diclosure thing towards the bottom of the the page

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -64,11 +64,9 @@ function App() {
           SEARCH
         </button>
         <div className="logo">cineMatch</div>
-        <button className="navigation-button">HELP</button>
         <button className="navigation-button">FEED</button>   {/* They both go nowhere rightnow */}
         <button className="navigation-button">WATCH LIST</button>
-        
-
+      
       </div>
 
       <div className="main-container">
@@ -92,22 +90,6 @@ function App() {
           </ul>
           <button className="go-btn" onClick={doSearch}>SEARCH</button>  {/* The button to actually search, this one is permanent */}
 
-          <footer className="sidebar-footer-credit">
-            <p>
-              Source of data:{" "}
-              <a href="https://www.themoviedb.org/">{/* We need this to abide by the TOS  */}
-                TMDB{" "}
-                <img
-                  src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
-                  style={{ height: "10px", width: "auto", verticalAlign: "middle", marginLeft: "6px" }}
-                  alt="TMDB logo"
-                />
-              </a>
-            </p>
-            <p>
-              This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.
-            </p>
-          </footer>
         </aside>
 
         <main className="content-area">
@@ -131,6 +113,22 @@ function App() {
           </div>
         </main>
       </div>
+        <footer className="sidebar-footer-credit">
+            <p>
+              Source of data:{" "}
+              <a href="https://www.themoviedb.org/">{/* We need this to abide by the TOS  */}
+                TMDB{" "}
+                <img
+                  src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+                  style={{ height: "10px", width: "auto", verticalAlign: "middle", marginLeft: "6px" }}
+                  alt="TMDB logo"
+                />
+              </a>
+            </p>
+            <p>
+              This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.
+            </p>
+          </footer>
     </>
   );
 }
