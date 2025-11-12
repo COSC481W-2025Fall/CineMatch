eefd0efe04e5a4e86eb45c4ae56a518643f8cf16
EricWill082902 <eeggles3@emich.edu>
2025-11-11 23:57:45 -0500

    Recommendation feed defaults from 10 to 20 movies

--- mern/client/src/components/RecommendationFeed.jsx
+++ mern/client/src/components/RecommendationFeed.jsx
@@ -5,7 +5,7 @@ import "../App.css";
 import MovieDetails from "./MovieDetails";
 
 const TMDB_IMG = "https://image.tmdb.org/t/p/w342";
-const DEFAULT_LIMIT = 10;
+const DEFAULT_LIMIT = 20; 
 
 export default function RecommendationFeed() {
     const watchedIds = useMemo(
