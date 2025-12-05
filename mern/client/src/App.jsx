10982da1b81bd4fdacf144be0d86bc7f0e22f4e3
BiggHiggs <higgsthecat@gmail.com>
2025-12-04 19:11:00 -0500

    TMDB link opens a new tab now when clicked

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -903,7 +903,11 @@ function App() {
                     <footer className="sidebar-footer-credit">
                         <p>
                             Source of data:{" "}
-                            <a href="https://www.themoviedb.org/">
+                            <a
+                                href="https://www.themoviedb.org/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
                                 TMDB{" "}
                                 <img
                                     src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
